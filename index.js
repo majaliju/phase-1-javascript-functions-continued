d9ba2005837e4831de6bd25a02a365fe10f13a65
majaliju <majalijunyc@gmail.com>
2022-01-18 16:07:50 -0500

    commit tree 65d6834fe2ba97d3c151889ce83c5413745cd582

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+
+// write a function called saturdayFun
+function saturdayFun(activity) {
+    let message;
+    if (activity !== undefined) {
+        message = `This Saturday, I want to ${activity}!`;
+    }
+    else {
+        activity = "roller-skate";
+        message= `This Saturday, I want to ${activity}!`;
+    }
+    return message;
+}
+
+
+// implement a function expression called mondayWork
+const mondayWork = function(activity) {
+    let message;
+    if (activity !== undefined) {
+        message = `This Monday, I will ${activity}.`;
+    }
+    else {
+        activity = "go to the office";
+        message= `This Monday, I will ${activity}.`;
+    }
+    return message;
+}
+
+// implement a function called wrapAdjective
+function wrapAdjective(adj) {
+    const innerFunction = function(phrase){
+        return `You are ${adj}${phrase}${adj}!`;
+    }
+    return innerFunction;
+}
